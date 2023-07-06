70d23b0e2980dc82c523aec2799f92e6b7c4d2e9
ciw <ciw@n.com>
2023-07-06 15:35:51 +0900

    day14 생성

---
+++ src/generic/MainClass05.java
@@ -0,0 +1,24 @@
+package generic;
+
+import java.util.Scanner;
+
+public class MainClass05 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int num, result=0;
+		
+		Service05 mem = new ServiceImpl05();
+		
+		while(true) {
+			System.out.println("1. 회원관리");
+			System.out.println("2. 종료");
+			num = sc.nextInt();
+			switch(num) {
+			case 1: 
+				mem.display();
+				break;
+			case 2: return;
+			}
+		}
+	}
+}
